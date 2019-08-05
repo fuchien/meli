b5347d6c56b6b33d3ce8ec2bc90b1717de43b256
Chiien <fuchienhsu12@gmail.com>
2019-08-04 21:52:20 -0300

    Feat(pages/items.js) add items page

---
+++ pages/items.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+const Items = () => (
+    <div>
+        <Head>
+            <title>Items</title>
+        </Head>
+        <h1>Hello Items</h1>
+    </div>
+);
+
+export default Items;
