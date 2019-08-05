1b16fcb7c7b87b0a8d8dd6b3823306ddee75cf59
Chiien <fuchienhsu12@gmail.com>
2019-08-05 10:52:28 -0300

    Feat(pages/Items.js) add getInitialProps and api

--- pages/items.js
+++ pages/items.js
@@ -2,13 +2,25 @@ import React from 'react';
 
 import Head from 'next/head';
 
+// API
+import api from '../src/services/api';
+
+// CONTAINERS
+import ItemsUI from '../src/containers/Items';
+
 const Items = () => (
     <div>
         <Head>
             <title>Items</title>
         </Head>
-        <h1>Hello Items</h1>
+        <ItemsUI />
     </div>
 );
+
+Items.getInitialProps = async ({ query }) => {
+    const { data } = await api.get(`/sites/MLA/search?q=${query.search}`);
+    console.log(data);
+    return { data };
+};
 
 export default Items;
