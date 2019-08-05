540093fb25a45c79bf55766c4a5b3f931e10329f
Chiien <fuchienhsu12@gmail.com>
2019-08-04 21:53:26 -0300

    Feat(components/Header) add header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+// STYLED-COMPONENTS
+import { Container } from './styles';
+
+export default function Header({ handleSubmitForm, handleInputChange, value }) {
+    return (
+        <Container>
+            <div className="header">
+                <img
+                    className="header__img"
+                    src="/static/images/Logo_ML.png"
+                    alt="Mercado Livre"
+                />
+                <form onSubmit={handleSubmitForm}>
+                    <input
+                        autoFocus
+                        type="text"
+                        value={value}
+                        onChange={handleInputChange}
+                        className="header__input"
+                        placeholder="Nunca dejes de buscar"
+                    />
+                    <button type="submit">
+                        <img
+                            className="header__search"
+                            alt="Search icon"
+                            src="/static/images/ic_Search.png"
+                        />
+                    </button>
+                </form>
+            </div>
+        </Container>
+    );
+}
