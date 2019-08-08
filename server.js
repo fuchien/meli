3c68168ccfd7d9a1769e336b20d796c784732df5
Chiien <fuchienhsu12@gmail.com>
2019-08-08 08:52:48 -0300

    Feat(server.js) add PORT variable

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const { createServer } = require('http');
 const next = require('next');
 const routes = require('./routes');
 
+const PORT = process.env.PORT || 3001;
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 
 app.prepare()
     .then(() => {
-        createServer(handler).listen(3000);
+        createServer(handler).listen(PORT);
     })
     .catch(ex => {
         console.log(ex.stack);
