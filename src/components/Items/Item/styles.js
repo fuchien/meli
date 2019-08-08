cdba28ed08a0361e1783a1dece963a6e268d550e
Chiien <fuchienhsu12@gmail.com>
2019-08-08 08:56:01 -0300

    Feat(components/Items/Item/styles.js) add styles

---
+++ src/components/Items/Item/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .item__img {
+        max-width: 150px;
+        height: auto;
+        flex: 1;
+    }
+
+    .item__content {
+        align-self: flex-start;
+        margin-left: 20px;
+        display: flex;
+        flex-flow: column;
+        align-items: flex-start;
+        justify-content: space-between;
+        flex: 5;
+
+        .item__price,
+        .item__description {
+            margin-top: 10px;
+        }
+    }
+
+    .item__address {
+        display: flex;
+        flex: 1;
+    }
+`;
