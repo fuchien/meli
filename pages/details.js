7d711a50e71702054dc177cb61136825ab6410ac
Chiien <fuchienhsu12@gmail.com>
2019-08-09 23:18:32 -0300

    Feat(pages/details.js) add page

---
+++ pages/details.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+const Details = () => (
+    <div>
+        <Head>
+            <title>Details</title>
+        </Head>
+    </div>
+);
+
+export default Details;
