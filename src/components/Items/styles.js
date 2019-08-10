54f54053e72bdabac55f8632ef9e9385a18242b2
Chiien <fuchienhsu12@gmail.com>
2019-08-10 14:19:38 -0300

    Feat(components/Items/styles.js) add pageContainer

--- src/components/Items/styles.js
+++ src/components/Items/styles.js
@@ -1,21 +1,20 @@
 import styled from 'styled-components';
 
-import media from '../../global/media_screen';
+// CSS
+import { PageContainer } from '../../global/page_container';
 
-export const Container = styled.div`
-    width: 100vw;
-    height: calc(100% - 56px);
-    background-color: #eee;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    padding: 50px 20px;
-    ${media.SCREEN_1232`
-        padding: 50px 200px;
-    `}
-
+export const Container = styled(PageContainer)`
     .items {
         width: 100%;
         background-color: #fff;
+
+        li {
+            cursor: pointer;
+
+            &:hover {
+                transform: scale(1);
+                box-shadow: 14px 14px 30px 0px rgba(0, 0, 0, 0.3);
+            }
+        }
     }
 `;
