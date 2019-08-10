d5a513fb71d363dc6ea8296e6fd88bf336581209
Chiien <fuchienhsu12@gmail.com>
2019-08-10 14:16:55 -0300

    Feat(pages/details.js) add components and requests

--- pages/details.js
+++ pages/details.js
@@ -2,12 +2,34 @@ import React from 'react';
 
 import Head from 'next/head';
 
-const Details = () => (
-    <div>
+// API
+import api from '../src/services/api';
+
+// CONTAINERS
+import DetailsUI from '../src/containers/Details';
+
+const Details = props => (
+    <>
         <Head>
             <title>Details</title>
         </Head>
-    </div>
+        <DetailsUI {...props} />
+    </>
 );
+
+Details.getInitialProps = async ({ query: { id } }) => {
+    try {
+        const detailsResponse = await api.get(`/items/${id}`);
+        const descriptionResponse = await api.get(`/items/${id}/description`);
+        return {
+            details: detailsResponse.data,
+            description: descriptionResponse.data
+        };
+    } catch (err) {
+        return {
+            err
+        };
+    }
+};
 
 export default Details;
