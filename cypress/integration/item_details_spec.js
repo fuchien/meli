ce8f90f21cad5d46a314ce4eac51682fc1e9f169
Chiien <fuchienhsu12@gmail.com>
2019-08-09 23:18:15 -0300

    Feat(cypress/integrations/item_detail_spec.js) add e2e test

---
+++ cypress/integration/item_details_spec.js
@@ -0,0 +1,31 @@
+import resultJSON from '../fixtures/results.json';
+
+Cypress.on('uncaught:exception', (err, runnable) => false);
+describe('MELI Item Details page', () => {
+    beforeEach(() => {
+        cy.visit(`/items/${resultJSON.results[0].id}`);
+    });
+
+    it('should show page and item id on url', () => {
+        cy.location().should(loc => {
+            expect(loc.pathname).to.eq(`/items/${resultJSON.results[0].id}`);
+        });
+    });
+
+    it('should show all the item details', () => {
+        cy.get('.details__img')
+            .should('be.visible')
+            .get('.details__title')
+            .should('be.visible')
+            .get('.details__price')
+            .should('be.visible')
+            .get('.details__sold__qty')
+            .should('be.visible')
+            .get('.details__buy__button')
+            .should('be.visible')
+            .get('.details__description__title')
+            .should('be.visible')
+            .get('.details__description__text')
+            .should('be.visible');
+    });
+});
