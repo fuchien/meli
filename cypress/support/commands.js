4df31c4393f0a42e356cb55fe2bacb0001981feb
Chiien <fuchienhsu12@gmail.com>
2019-08-09 23:02:20 -0300

    Feat(cypress/support/commands.js) add new command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('takeResults', () => {
+    const typedText = 'Appleipod';
+    cy.server();
+    cy.fixture('results.json').as('resultsJSON');
+    cy.route('GET', '**/sites/MLA/**', '@resultsJSON');
+    cy.visit(`/items/?search=${typedText}`);
+});
