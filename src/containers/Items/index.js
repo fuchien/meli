3cf1994952e9211f9ad9a5f56909cc2d2fb99eae
Chiien <fuchienhsu12@gmail.com>
2019-08-09 23:04:47 -0300

    Feat(containers/Items) add handlerClick event to change route

--- src/containers/Items/index.js
+++ src/containers/Items/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Router } from '../../../routes';
 
 // COMPONENTS
 import ItemsUI from '../../components/Items';
 
 const Items = ({ data }) => {
-    return <ItemsUI data={data} />;
+    const handleItemClicked = id => {
+        Router.pushRoute(`/items/${id}`);
+    };
+
+    return <ItemsUI handleItemClicked={handleItemClicked} data={data} />;
 };
 
 export default Items;
