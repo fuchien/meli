1f6e48734006eca717c000982247df3a4700d561
Chiien <fuchienhsu12@gmail.com>
2019-08-14 11:48:31 -0300

    Feat(pages/items.js) add propTypes

--- pages/items.js
+++ pages/items.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
 
 // API
 import api from '../src/services/api';
 
 // CONTAINERS
 import ItemsUI from '../src/containers/Items';
+import CategoriesUI from '../src/containers/Categories';
 
 const Items = ({ data, errMsg }) => (
     <>
         <Head>
             <title>Items</title>
         </Head>
+        <CategoriesUI categories={data.categories} />
         <ItemsUI data={data} errMsg={errMsg} />
     </>
 );
+
+Items.defaultProps = {
+    data: null,
+    errMsg: null
+};
+
+Items.propTypes = {
+    data: PropTypes.shape(),
+    errMsg: PropTypes.string
+};
 
 Items.getInitialProps = async ({ query }) => {
     if (!query.search) {
