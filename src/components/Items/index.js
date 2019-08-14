81132533287cb043b3f6ea654abeedd127635d6b
Chiien <fuchienhsu12@gmail.com>
2019-08-14 11:50:31 -0300

    Feat(components/Items) add propTypes

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // STYLED-COMPONENTS
 import { Container } from './styles';
@@ -6,28 +7,31 @@ import { Container } from './styles';
 // COMPONENTS
 import Item from './Item';
 
-const Items = ({ data, handleItemClicked }) => {
-    return (
-        <Container>
-            <div className="items">
-                {data.results.length ? (
-                    <ul className="items__list">
-                        {data.results.map(result => (
-                            <li
-                                key={result.id}
-                                onClick={() => handleItemClicked(result.id)}
-                                className="items__list__item"
-                            >
-                                <Item {...result} />
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p className="items__empty__error">Nenhum item achado</p>
-                )}
-            </div>
-        </Container>
-    );
+const Items = ({ data, handleItemClicked }) => (
+    <Container>
+        <div className="items">
+            {data.items.length ? (
+                <ul className="items__list">
+                    {data.items.map(result => (
+                        <li
+                            key={result.id}
+                            onClick={() => handleItemClicked(result.id)}
+                            className="items__list__item"
+                        >
+                            <Item result={result} />
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p className="items__empty__error">Nenhum item achado</p>
+            )}
+        </div>
+    </Container>
+);
+
+Items.propTypes = {
+    data: PropTypes.shape().isRequired,
+    handleItemClicked: PropTypes.func.isRequired
 };
 
 export default Items;
