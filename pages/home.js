e78dd54ea588afed415d1c3671a6329c80c5e245
Chiien <fuchienhsu12@gmail.com>
2019-08-14 16:11:39 -0300

    Feat(pages/home.js) add store and loader

--- pages/home.js
+++ pages/home.js
@@ -2,15 +2,34 @@ import React from 'react';
 
 import Head from 'next/head';
 
-const Home = () => (
-    <div>
-        <Head>
-            <title>Home</title>
-        </Head>
-        <p style={{ textAlign: 'center', marginTop: '350px' }}>
-            Bem vindo ao Mercado Livre
-        </p>
-    </div>
-);
+// STORE
+import Connect from '../src/store/config/connect';
 
-export default Home;
+// COMPONENTS
+import Loader from '../src/components/UI/Loader';
+
+const Home = ({ loading }) => {
+    return (
+        <div>
+            <Head>
+                <title>Home</title>
+            </Head>
+            {loading ? (
+                <Loader />
+            ) : (
+                <p style={{ textAlign: 'center', marginTop: '350px' }}>
+                    Bem vindo ao Mercado Livre
+                </p>
+            )}
+        </div>
+    );
+};
+
+const mapStateToProps = ({ loading }, props) => {
+    return {
+        loading,
+        ...props
+    };
+};
+
+export default Connect(mapStateToProps)(Home);
