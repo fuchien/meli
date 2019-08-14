6c2852dfb15eb16c839f0a093b646d8fc4d60a40
Chiien <fuchienhsu12@gmail.com>
2019-08-14 11:51:13 -0300

    Feat(containers/Categories) add component

---
+++ src/containers/Categories/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// COMPONENTS
+import CategoriesUI from '../../components/Categories';
+
+const Categories = ({ categories }) => <CategoriesUI categories={categories} />;
+
+export default Categories;
