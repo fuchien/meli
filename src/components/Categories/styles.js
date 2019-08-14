068af9762f6ade07bc46574ce28ea5f8e44533f9
Chiien <fuchienhsu12@gmail.com>
2019-08-14 11:49:26 -0300

    Feat(components/Categories/styles.js) add styles

---
+++ src/components/Categories/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+import media from '../../global/media_screen';
+
+export const Container = styled.div`
+    min-width: 100%;
+    padding: 20px 0 0 20px;
+    background-color: #eee;
+    ${media.SCREEN_1232`
+        padding: 20px 200px 0 200px;
+    `}
+
+    .items__categories {
+        display: flex;
+        .category__arrow {
+            margin: 0 10px;
+        }
+    }
+`;
