56829002077c6fb47801a1b26309389bb106dcb6
Chiien <fuchienhsu12@gmail.com>
2019-08-14 11:50:53 -0300

    Feat(components/Items/Item) add propTypes

--- src/components/Items/Item/index.js
+++ src/components/Items/Item/index.js
@@ -1,21 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // STYLED-COMPONENTS
 import { Container } from './styles';
 
-const Item = ({ thumbnail, title, price, address: { state_name } }) => {
-    return (
-        <Container>
-            <img className="item__img" alt={title} src={thumbnail} />
-            <div className="item__content">
-                <h5 className="item__price">$ {price}</h5>
-                <p className="item__description">{title}</p>
-            </div>
-            <div className="item__address">
-                <p className="item__address__state">{state_name}</p>
-            </div>
-        </Container>
-    );
+const Item = ({
+    result: {
+        picture,
+        title,
+        price,
+        address: { state_name }
+    }
+}) => (
+    <Container>
+        <img className="item__img" alt={title} src={picture} />
+        <div className="item__content">
+            <h5 className="item__price">$ {price.amount}</h5>
+            <p className="item__description">{title}</p>
+        </div>
+        <div className="item__address">
+            <p className="item__address__state">{state_name}</p>
+        </div>
+    </Container>
+);
+
+Item.propTypes = {
+    result: PropTypes.shape({
+        address: PropTypes.shape({
+            state_name: PropTypes.string
+        }).isRequired,
+        picture: PropTypes.string.isRequired,
+        price: PropTypes.shape({
+            amount: PropTypes.number
+        }).isRequired,
+        title: PropTypes.string.isRequired
+    }).isRequired
 };
 
 export default Item;
