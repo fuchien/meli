bd8128da1d7b7f0221163dd57c3c40b322e12adf
Chiien <fuchienhsu12@gmail.com>
2019-08-14 16:09:49 -0300

    Feat(components/UI/Loader) add loader component

---
+++ src/components/UI/Loader/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// STYLED-COMPONENTS
+import { Container } from './styles';
+
+const Loader = () => (
+    <Container>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </Container>
+);
+
+export default Loader;
