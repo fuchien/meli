b6afd2543a196bb2fa5442f7264eb5dc26e5ee1a
Chiien <fuchienhsu12@gmail.com>
2019-08-14 11:50:44 -0300

    Feat(components/Items/styles.js) change styles

--- src/components/Items/styles.js
+++ src/components/Items/styles.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
+import media from '../../global/media_screen';
+
 // CSS
 import { PageContainer } from '../../global/page_container';
 
 export const Container = styled(PageContainer)`
+    padding-top: 20px;
+    ${media.SCREEN_1232`
+        padding-top: 20px;
+    `}
     .items {
         width: 100%;
         background-color: #fff;
@@ -15,6 +21,11 @@ export const Container = styled(PageContainer)`
                 transform: scale(1);
                 box-shadow: 14px 14px 30px 0px rgba(0, 0, 0, 0.3);
             }
+        }
+
+        .items__empty__error {
+            text-align: center;
+            margin-top: 350px;
         }
     }
 `;
